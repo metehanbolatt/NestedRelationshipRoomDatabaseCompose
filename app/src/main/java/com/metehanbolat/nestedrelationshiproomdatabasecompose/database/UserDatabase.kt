04f9f1b212eff2541d4b1d74b0e69ee94a90bad0
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-06 13:59:10 +0300

    Room database was created.

---
+++ app/src/main/java/com/metehanbolat/nestedrelationshiproomdatabasecompose/database/UserDatabase.kt
@@ -0,0 +1,35 @@
+package com.metehanbolat.nestedrelationshiproomdatabasecompose.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.metehanbolat.nestedrelationshiproomdatabasecompose.entity.PlayList
+import com.metehanbolat.nestedrelationshiproomdatabasecompose.entity.PlayListSongCrossRef
+import com.metehanbolat.nestedrelationshiproomdatabasecompose.entity.Song
+import com.metehanbolat.nestedrelationshiproomdatabasecompose.entity.User
+
+@Database(entities = [User::class, PlayList::class, Song::class, PlayListSongCrossRef::class], version = 1, exportSchema = false)
+abstract class UserDatabase: RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getInstance(context: Context): UserDatabase {
+            val userInstance = INSTANCE
+            if (userInstance != null) return userInstance
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "user_database"
+                ).allowMainThreadQueries().build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
